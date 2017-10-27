ad2d62465433a39c95fac36eebc4bdff62733999
Nicole Carvalho <NicoleCarvalho@NicoleCalhosMBP.home>
2017-10-26 21:59:20 -0400

    edited the port for deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdb";
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
